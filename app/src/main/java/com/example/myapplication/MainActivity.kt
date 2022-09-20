63fca16bcfade81bc8994d75bd347755e6947eee
tsubasaMiyamae <mitu_nari@hotmail.co.jp>
2022-09-20 11:21:18 +0900

    miyamaeCommit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -29,4 +29,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+    // aaaaa
+    // aaaa
+
 }
